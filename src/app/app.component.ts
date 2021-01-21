4a4bb6da8226bfb0eba76f5cfdec26804b862f17
mellexmas <empauhe@gmail.com>
2021-01-21 17:05:11 +0100

    fin de journée clear

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,30 +10,7 @@ export class AppComponent {
   title = 'todo-list';
 
 
-  public todoArray: Array<TodoItem> = [
-    {
-      title : 'item 1',
-      checked: true,
-    },
-    {
-      title : 'item 2',
-      checked: true,
-    },
-    {
-      title : 'item 3',
-      checked: true,
-    },
-    {
-      title : 'item 4',
-      checked: false,
-    },
-    {
-      title : 'toto',
-      checked: false,
-    }
-    
-    
-  ];
+  public todoArray: Array<TodoItem> = [];
 
   public addItem($event: KeyboardEvent): void {
     if ($event.code === 'Enter') {
@@ -46,8 +23,8 @@ export class AppComponent {
 
         if (str !== '') {
           this.todoArray.push({
-            title : str,
-            checked : false,
+            title: str,
+            checked: false,
           });
           $input.value = '';
         }
@@ -63,19 +40,36 @@ export class AppComponent {
   }
 
 
-public removeItem(index : number): void {
-  //$event:MouseEvent : pas utilisé alors on peut enlever et du html aussi
-  //const index = this.todoArray.indexOf(item);//recupere l'index du 1er element trouvé
- // console.log(item);
-  if (index !== -1) {
-    this.todoArray.splice(index, 1); //enleve un ou pls elemts dans table index du 1er elem a suppr et le nb a suppr a partir de la
+  public removeItem(index: number): void {
+    //$event:MouseEvent : pas utilisé alors on peut enlever et du html aussi
+    //const index = this.todoArray.indexOf(item);//recupere l'index du 1er element trouvé
+    // console.log(item);
+    if (index !== -1) {
+      this.todoArray.splice(index, 1); //enleve un ou pls elemts dans table index du 1er elem a suppr et le nb a suppr a partir de la
+    }
   }
-}
   /* public clickEvent() {
     console.log("j'ai récup le click");
     this.todoArray.pop();
-  }*/ 
+  }*/
 
+  public clearCompleted(): void {
+    //console.log("click récupéré");
+    //SOLUCE AYMERIC
+    /* for (let i =0 ; i < this.todoArray.length -1; i > = 0 ; i--){
+       //boucle à l'envers on part de la fin du tabeau comme ça les ID ne foirent pas
+      if(this.todoArray[i].checked === true){
+        this.todoArray.splice(i, 1);
+      }
+    } */
+    //3eme méthode FILTER
+    this.todoArray = this.todoArray.filter((item: TodoItem) => {
+      return !item.checked;
+      //return true if you want to keep the element in the new array
+      //return false if you want to remove the element from the new array.
+    })
+
+  }
 
 
 }
