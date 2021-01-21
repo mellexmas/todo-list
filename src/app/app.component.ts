7b6d2b52fe5797ddfc277402c0d650102909bb14
mellexmas <empauhe@gmail.com>
2021-01-21 11:49:08 +0100

    add remove last item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,12 +20,17 @@ export class AppComponent {
   public addItem($event: KeyboardEvent): void {
     if ($event.code === 'Enter') {
       //const $input: HTMLInputElement|null = $event.target as HTMLInputElement;
-      //
 
       const $input: EventTarget | null = $event.target;
 
       if ($input instanceof HTMLInputElement) {
-        console.log($input.value);
+        const str = $input.value.trim();
+
+        if (str !== '') {
+          this.todoArray.push($input.value);
+          $input.value = '';
+        }
+
       }
       console.log("enter pressed");
       /*if ($input !== null) {
@@ -35,5 +40,21 @@ export class AppComponent {
     }
 
   }
+
+
+public removeItem($event: MouseEvent, item:string): void {
+  //$event:MouseEvent : pas utilisé alors on peut enlever et du html aussi
+  const index = this.todoArray.indexOf(item);//recupere l'index du 1er element trouvé
+  console.log(item);
+  if (index !== -1) {
+    this.todoArray.splice(index, 1); //enleve un ou pls elemts dans table index du 1er elem a suppr et le nb a suppr a partir de la
+  }
+}
+  /* public clickEvent() {
+    console.log("j'ai récup le click");
+    this.todoArray.pop();
+  }*/ 
+
+
 
 }
