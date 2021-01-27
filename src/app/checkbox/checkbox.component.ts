353f032d069b134d033552870704e2e00b4e91b8
mellexmas <empauhe@gmail.com>
2021-01-27 11:25:24 +0100

    add label attribute to web compenent

--- src/app/checkbox/checkbox.component.ts
+++ src/app/checkbox/checkbox.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-checkbox',
@@ -8,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class CheckboxComponent implements OnInit {
   private static counter = 0;
   public id: string;
+  //ce decorator decorate nous label attribute . add info for angular;
+  //permet de dire que l'attribut que j'ai la je peux l'utiliser dans la balise html du web component qu'on crée
+  @Input()
+  public label: string|null= null;
 
   constructor() { 
     this.id = 'app-checkbox-' + CheckboxComponent.counter; //nom de classe.nomattribut
