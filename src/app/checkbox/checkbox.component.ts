b06cfd4da2a69d8307d7f820a272c4bd0258b244
mellexmas <empauhe@gmail.com>
2021-01-27 10:17:19 +0100

    id random for checkbox compenent

--- src/app/checkbox/checkbox.component.ts
+++ src/app/checkbox/checkbox.component.ts
@@ -6,8 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkbox.component.scss']
 })
 export class CheckboxComponent implements OnInit {
+ 
+  public id: string;
 
-  constructor() { }
+  constructor() { 
+    let rand = Math.random();
+    rand = rand * 1000000;
+    rand = Math.round(rand); //arrondi à l'entier le plus proche
+
+    this.id = 'app-checkbox-' + rand;
+  }
+
+
+
+
 
   ngOnInit(): void {
   }
